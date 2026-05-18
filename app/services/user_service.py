983cf656f750332da3a168277f2c5d88c389e222
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-05-18 16:14:56 -0300

    feat: cria funcao create user para criar usuario

---
+++ app/services/user_service.py
@@ -0,0 +1,12 @@
+from app.database.supabase import get_supabase_client
+from app.schemas.user import UserCreate
+
+supabase = get_supabase_client
+
+
+def create_user(user: UserCreate):
+    user_dict = user.model_dump()
+
+    response = supabase.table("users").insert(user_dict).execute()
+
+    return response.data[0]
