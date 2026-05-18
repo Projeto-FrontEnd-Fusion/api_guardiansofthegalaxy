8c10489fbe6c2bc326645b24a499c32464226486
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-05-18 16:58:43 -0300

    fix: corrige parametro do model dump para serializar todo o objeto em string

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,11 +1,11 @@
 from app.database.supabase import get_supabase_client
 from app.schemas.user import UserCreate
 
-supabase = get_supabase_client
+supabase = get_supabase_client()
 
 
 def create_user(user: UserCreate):
-    user_dict = user.model_dump()
+    user_dict = user.model_dump(mode="json")
 
     response = supabase.table("users").insert(user_dict).execute()
 
