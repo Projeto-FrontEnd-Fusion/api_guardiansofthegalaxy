7752a77aab7ce5852897b43ddd4a8e9f05336130
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-05-19 17:33:49 -0300

    chore: cria classe base para exceptions

--- app/core/exceptions/user.py
+++ app/core/exceptions/user.py
@@ -1,3 +1,10 @@
-class UserCreationError(Exception):
+class AppException(Exception):
+    detail = "Erro de aplicação"
+    error_code = "APPLICATION_ERROR"
+    status_code = 500
+
+
+class UserCreationError(AppException):
     detail = "Usuário não pode ser criado"
     error_code = "USER_NOT_CREATED"
+    status_code = 500
