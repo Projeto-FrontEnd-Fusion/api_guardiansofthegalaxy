949953c2018bffae3e659a98dfdf2950965e83c5
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2026-07-22 22:56:57 -0300

    feat: criar services para criar depoimento e validar entrada do usuario e criar diretorio  utils

---
+++ app/services/testimonials_service.py
@@ -0,0 +1,34 @@
+from better_profanity import profanity
+from app.utils.bad_words import bad_w
+
+from app.core.exceptions import TestimonialContentBlocked, TestimonialError
+from app.repositories.testimonial_repository import (
+    create_testimonial_repository,
+    list_testimonials_repository,
+)
+from app.schemas.testimonial import TestimonialsSchema
+
+
+
+profanity.load_censor_words(bad_w)
+
+
+def create_testimonial(testimonial: TestimonialsSchema):
+    try:
+        if profanity.contains_profanity(testimonial.name) or \
+           profanity.contains_profanity(testimonial.testimonial):
+            raise TestimonialContentBlocked()
+
+        return create_testimonial_repository(testimonial)
+
+    except TestimonialContentBlocked:
+        raise TestimonialContentBlocked
+    except Exception:
+        raise TestimonialError()
+
+
+def list_testimonials():
+    try:
+        return list_testimonials_repository()
+    except Exception:
+        raise TestimonialError()
